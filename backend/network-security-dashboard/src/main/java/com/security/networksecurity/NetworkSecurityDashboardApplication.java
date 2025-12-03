23f892f0f1453b5feadd9306a913b4569e61e32a
AdityaSharma0410 <melee.zeno@gmail.com>
2025-12-03 23:27:29 +0530

    Backend dependencies added

---
+++ backend/network-security-dashboard/src/main/java/com/security/networksecurity/NetworkSecurityDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.security.networksecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkSecurityDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkSecurityDashboardApplication.class, args);
+	}
+
+}
---
+++ backend/network-security-dashboard/src/test/java/com/security/networksecurity/NetworkSecurityDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.networksecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkSecurityDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
